a837c1acf57fa69b739a3346850d3efd63c62a23
Gustavo Faria <gustavofaria985@gmail.com>
2025-09-05 17:04:37 -0300

    Fazendo o funcionar a edição do fornecedor

--- routes/dados-fornecedor.js
+++ routes/dados-fornecedor.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const fornecedorController = require('../controllers/fornecedorController');
 
 router.get('/' ,fornecedorController.listar);
+router.post('/fornecedores/editar/:id', fornecedorController.editar);
+router.post('/fornecedores/deletar/:id', fornecedorController.deletar);
 
 module.exports = router;
--- routes/fornecedores.js
+++ routes/fornecedores.js
@@ -2,5 +2,8 @@ const router = require('express').Router();
 const fornecedorController = require('../controllers/fornecedorController');
 
 router.post('/create', fornecedorController.create);
+router.post('/editar/:id', fornecedorController.editar);
+router.post('/deletar/:id', fornecedorController.deletar);
+
 
 module.exports = router;
